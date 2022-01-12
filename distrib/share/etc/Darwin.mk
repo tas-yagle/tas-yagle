72ff93110050cf350b20475b57d6eaa0975144fa
Rob Taylor <robtaylor@floopily.org>
2022-01-12 06:25:06 +0000

    Use brew for non standard darwin components

--- distrib/share/etc/Darwin.mk
+++ distrib/share/etc/Darwin.mk
@@ -12,14 +12,14 @@
 
 PROGRAM_SUFFIX=
 
-GNU_LIB          = /usr/local/lib
-GNU_INCLUDE      = /usr/local/include
+GNU_LIB          =
+GNU_INCLUDE      =
 
 X11_LIB          = /usr/X11R6/lib/
-X11_INCLUDE      = /usr/X11R6/include 
+X11_INCLUDE      = /usr/X11R6/include
 
-MOTIF_LIB        = /usr/OpenMotif/lib
-MOTIF_INCLUDE    = /usr/OpenMotif/include
+MOTIF_LIB        = $(shell brew --prefix openmotif)/lib
+MOTIF_INCLUDE    = $(shell brew --prefix openmotif)/include
 
 XPM_LIB          = /usr/X11R6/lib
 XPM_INCLUDE      = /usr/X11R6/include
@@ -43,11 +43,11 @@ RANLIB		= /usr/bin/ranlib
 MAKE             = /usr/bin/make
 MAKEFLAGS        = 
 
-CC               = /usr/bin/gcc -L/usr/local/lib -I/usr/local/include -fno-common
-SCC              = /usr/bin/gcc -I/usr/local/include -fno-common -Xlinker -undefined -Xlinker suppress -Xlinker -flat_namespace -dynamiclib
+CC               = /usr/bin/gcc -fno-common
+SCC              = /usr/bin/gcc -fno-common -Xlinker -undefined -Xlinker suppress -Xlinker -flat_namespace -dynamiclib
 CPLUSPLUS        = /usr/bin/g++
 CFLAGS           = -O3
-CPPFLAGS         = 
+CPPFLAGS         ?= 
 
 OPTIM            = -O2 -Wall
 
@@ -56,15 +56,16 @@ PURIFY           = purify
 YACC             = /usr/bin/bison
 YACCFLAGS        = -y 
 
-LEX              = /usr/local/bin/flex
+LEX              = /usr//bin/lex
 LEXFLAGS         = 
 
 AR               = /usr/bin/ar
 ARFLAGS          = rv
 
-SWIG             = /usr/local/bin/swig
+SWIG             = $(shell brew --prefix swig)/bin/swig
 
-TCL_L            = -ltcl8.5
+TCL_H            = -L$(shell brew --prefix tcl-tk)/include
+TCL_L            = -L$(shell brew --prefix tcl-tk)/lib -ltcl8.6
 
 WHOLE            = -all_load
 NOWHOLE          = 
